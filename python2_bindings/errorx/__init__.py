efc572b28fea1d67f34132f0f6c2b1e906d3dc64
Alex Sevy <alex.sevy@gmail.com>
2019-02-06 09:13:42 -0500

    redid docstrings in python2 init.py

--- python2_bindings/errorx/__init__.py
+++ python2_bindings/errorx/__init__.py
@@ -1,43 +1,43 @@
+'''
+    ErrorX bindings for Python. This file provides an interface
+    for using the ErrorX dynamically linked library in a Python
+    package
+'''
 import errorx_lib
 
 from ErrorXOptions import ErrorXOptions
 import os
 
-'''
-	ErrorX bindings for Python. This file provides an interface
-	for using the ErrorX dynamically linked library in a Python
-	package
-'''
 
-'''
+def _get_base():
+	'''
 	Get the base path where ErrorX is located
 
 	@return base path 
-'''
-def _get_base():
+	'''
 	return os.path.dirname(errorx_lib.__file__)
 
-'''
-	Error correct the provided sequences. All parameters
-	are matched up elementwise, so they must have the same
-	length.
 
-	@param sequences List of NT sequences to correct
-	@param germline_sequences List of inferred germline sequences
-	corresponding to the entries in `sequences`
-	@param phred_scores List of PHRED quality scores straight
-	from the sequencer, corresponding to the entries in `sequences`
-	@param options ErrorXOptions object to control options. If 
-	None, will set up default values internally
-
-	@return List of corrected NT sequences corresponding
-	to the input param `sequences`
-'''
 def correct_sequences(sequences, 
 			   germline_sequences,
 			   phred_scores,
 			   options=None ):
+	'''
+    Error correct the provided sequences. All parameters
+    are matched up elementwise, so they must have the same
+    length.
 
+    @param sequences List of NT sequences to correct
+    @param germline_sequences List of inferred germline sequences
+    corresponding to the entries in `sequences`
+    @param phred_scores List of PHRED quality scores straight
+    from the sequencer, corresponding to the entries in `sequences`
+    @param options ErrorXOptions object to control options. If
+    None, will set up default values internally
+
+    @return List of corrected NT sequences corresponding
+    to the input param `sequences`
+	'''
 	## if input is a string, wrap into a list
 	if isinstance( sequences, str ):
 		sequences = [sequences]
@@ -59,7 +59,12 @@ def correct_sequences(sequences,
 		list(germline_sequences),
 		list(phred_scores), options )
 
-'''
+
+def get_predicted_errors(sequence, 
+			   germline_sequence,
+			   phred_score,
+			   options=None ):
+	'''
 	Get the predicted likelihood that each base along a NT sequence
 	is an error. 
 
@@ -71,12 +76,7 @@ def correct_sequences(sequences,
 
 	@return Python list of probability of error for each base, 
 	where position i is prob error at position i
-'''
-def get_predicted_errors(sequence, 
-			   germline_sequence,
-			   phred_score,
-			   options=None ):
-	
+	'''
 	if not options:
 		options = ErrorXOptions( "tmp", "tsv" )
 
@@ -84,12 +84,13 @@ def get_predicted_errors(sequence,
 	return errorx_lib.get_predicted_errors( 
 		str(sequence), str(germline_sequence), str(phred_score), options )
 
-'''
+
+def run_protocol( options ):
+	'''
 	Run the ErrorX protocol and write the results to a file
 
 	@param options ErrorXOptions object with the processing details
-'''
-def run_protocol( options ):
+	'''
 	## When the user doesn't set infile it defaults to 'tmp'
 	## if they're reading from a file this is not good
 	if options.infile == 'tmp':
