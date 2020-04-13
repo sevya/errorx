5416e011aef5ec1d6c006c83543ee64b2a95b059
Alex Sevy <alex.sevy@gmail.com>
2020-04-13 11:22:31 -0400

    force failure just to see what happens

--- unit_test/TestErrorX.hh
+++ unit_test/TestErrorX.hh
@@ -37,6 +37,10 @@ public:
 	}
 
 	void testFromOptions() {
+
+		// FAIL PURPOSELY
+		TS_ASSERT_EQUALS( 1, 0 );
+
 		ErrorXOptions options( "testing/test.fastq", "fastq" );
 		options.outfile( "testing/test_out.tsv");
 		options.species( "mouse" );
